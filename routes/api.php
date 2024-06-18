a4f24bd057a1e1b920cbe411513c1500d569ae4e
Chandrikadhina <chandrika.oktavian9@smk.belajar.id>
2024-06-18 14:03:04 +0700

    edit routes api

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('kategori',KategoriController::class);
+Route::apiResource('kategori', KategoriController::class);
